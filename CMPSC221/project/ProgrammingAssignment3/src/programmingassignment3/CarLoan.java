daf056a12f67398192bf65b30314111f5c0dc4d8
Xuhong Lin <112525393+pppmouren@users.noreply.github.com>
2024-04-02 14:18:35 -0400

    cmpsc221 project parriallt upload

---
+++ CMPSC221/project/ProgrammingAssignment3/src/programmingassignment3/CarLoan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programmingassignment3;
+
+/**
+ *
+ * @author handsomedad
+ */
+public class CarLoan extends LoanAccount{
+    private String vehicleVIN;
+    
+    public CarLoan(double principal, double annualInterestRate, int months, String vehicleVIN) {
+        super(principal, annualInterestRate, months);
+        this.vehicleVIN = vehicleVIN;
+    }
+    
+    public String getvehicleVIN() {
+        return this.vehicleVIN;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("%s\n%s\n%s %s\n\n\n", "Car Loan with:", super.toString(), "Vehicle VIN:", getvehicleVIN());
+    }
+}
